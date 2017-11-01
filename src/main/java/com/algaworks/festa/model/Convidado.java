053b32329056dd9b79bc45f47531d8ebf0215b92
higorbhermes <higorbhermes@gmail.com>
2017-11-01 00:48:21 -0200

    Topico 3.8

--- src/main/java/com/algaworks/festa/model/Convidado.java
+++ src/main/java/com/algaworks/festa/model/Convidado.java
@@ -1,5 +1,74 @@
 package com.algaworks.festa.model;
 
-public class Convidado {
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-}
+@Entity
+public class Convidado implements Serializable {
+	
+ 	private static final long serialVersionUID = 1L;
+ 
+ 	@Id
+ 	@GeneratedValue
+ 	private Long id;
+ 	
+ 	private String nome;
+ 	private Integer quantidadeAcompanhantes;
+ 	public Long getId() {
+		return id;
+ 	}
+ 	public void setId(Long id) {
+ 		this.id = id;
+ 	}
+ 	public String getNome() {
+ 		return nome;
+ 	}
+ 	public void setNome(String nome) {
+ 		this.nome = nome;
+	}
+	public Integer getQuantidadeAcompanhantes() {
+ 		return quantidadeAcompanhantes;
+ 	}
+ 	public void setQuantidadeAcompanhantes(Integer quantidadeAcompanhantes) {
+ 		this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+ 	}
+ 	
+ 	public Convidado(String nome, Integer quantidadeAcompanhantes) {
+ 		super();
+ 		this.nome = nome;
+ 		this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+ 	}
+ 	public Convidado() {
+ 		
+ 	}
+ 	@Override
+ 	public int hashCode() {
+ 		final int prime = 31;
+ 		int result = 1;
+ 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+ 		return result;
+ 	}
+ 	@Override
+ 	public boolean equals(Object obj) {
+ 		if (this == obj)
+ 			return true;
+ 		if (obj == null)
+ 			return false;
+ 		if (getClass() != obj.getClass())
+ 			return false;
+ 		Convidado other = (Convidado) obj;
+ 		if (id == null) {
+ 			if (other.id != null)
+ 				return false;
+ 		} else if (!id.equals(other.id))
+ 			return false;
+ 		return true;
+ 	}
+ 	
+ 	@Override
+ 	public String toString() {
+ 		return "Convidado [id=" + id + ", nome=" + nome + ", quantidadeAcompanhantes=" + quantidadeAcompanhantes + "]";
+ 	}
+ }
--- src/main/java/com/algaworks/festa/repository/Convidados.java
+++ src/main/java/com/algaworks/festa/repository/Convidados.java
@@ -1,5 +1,7 @@
 package com.algaworks.festa.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.algaworks.festa.model.Convidado;
 
-public class Convidados {
 
+public interface Convidados extends JpaRepository<Convidado, Long> {
 }
